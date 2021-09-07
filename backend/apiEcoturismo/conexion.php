8ad7fae214fc6e084be7c29b44f6f851371ebfad
Betsy Nazareno <betsy.nazareno@devsu.com>
2021-09-06 21:43:43 -0700

    pass

--- backend/apiEcoturismo/conexion.php
+++ backend/apiEcoturismo/conexion.php
@@ -7,7 +7,7 @@ function obtenerConexion(){
     $hostname   = "127.0.0.1";
     $db         = "ecoturismo";
     $usuario    = "root";
-    $contrasena = "";
+    $contrasena = "root";
     
     $link = mysqli_connect( $hostname,
                             $usuario,
